c170912bc60df94be003e04c6b2f32da73ad2801
Triumph <61302651+TriumphHu@users.noreply.github.com>
2020-11-23 14:45:18 +0800

    括号匹配的检验
    括号匹配的检验
    描述：
    假设一个表达式或一段程序中含有三种括号：圆括号“（”和“）”、方括号“[”和“]”、花括号“{”和“}”。试写一个程序判别给定的表达式或程序中所含括号是否正确配对出现。
    输入说明：
    每行为一个含有括号的表达式或一段程序。 
    输出说明：
    输出一个right或wrong，表明正确匹配与否。
    输入样例：
    while (m<(a[8]+t) {m=m+1;  t=t-1;}
    输出样例：
    wrong
    提示
    用栈来实现

---
+++ XDU数据结构题/xdoj系列/xdoj307.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+bool brackMatch(char brack[], int len); //检验传入的括号数组是否匹配，若能匹配返回true 1；否则返回false 0；
+int main()
+{
+	int n = 0;
+	char s[10000] = {};
+	char brack[1000] = {};
+	char ch;
+	int i = 0;
+	cin.getline(s, 10000);
+	while (s[i])
+	{
+		ch = s[i];
+		if (ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}')
+		{
+			brack[n] = (char)ch;
+			n++;
+		}
+		i++;
+	}
+	bool flag;
+	flag = brackMatch(brack, n);
+	if (flag)
+		cout << "right" << endl;
+	else
+		cout << "wrong" << endl;
+	return 0;
+}
+bool brackMatch(char brack[], int len)
+{
+	stack<char> stk;
+	for (int i = 0; i < len; i++)
+	{
+		int flag = 0;
+		switch (brack[i])
+		{
+		case '}':
+			if (!stk.empty() && stk.top() == '{')
+			{
+				stk.pop();
+				break;
+			}
+		case '>':
+			if (!stk.empty() && stk.top() == '<')
+			{
+				stk.pop();
+				break;
+			}
+		case ']':
+			if (!stk.empty() && stk.top() == '[')
+			{
+				stk.pop();
+				break;
+			}
+		case ')':
+			if (!stk.empty() && stk.top() == '(')
+			{
+				stk.pop();
+				break;
+			}
+		default:
+			stk.push(brack[i]);
+		}
+	}
+	if (stk.size() == 0)
+	{
+		return true;
+	}
+	return false;
+}
