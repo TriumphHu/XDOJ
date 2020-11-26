8b38abe51f97c0c4c5b0e7c5d4753162fe2eb232
Triumph <61302651+TriumphHu@users.noreply.github.com>
2020-11-27 01:10:09 +0800

    完全二叉树的子树
    题目：完全二叉树的子树
    问题描述
    对一棵完全二叉树，采用自上而下、自左往右的方式从1开始编号，我们已知这个二叉树的最后一个结点是n，现在的问题是结点m所在的子树一共包括多少个结点？
    输入格式
    	输入数据包括多行，每行给出一组测试数据，包括两个整数m，n (1 <= m <= n <= 1000000000)。0 0表示输入结束。
    输出格式
    	对于每一组测试数据，输出一行，该行包含一个整数，给出结点m所在子树中包括的结点的数目。
    样例输入
    	3 12
        0 0
    样例输出
    	4

---
+++ XDU数据结构题/xdoj系列/xdoj314.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+//µÝ¹éÅÐ¶ÏÊÇ·ñÓÐ×óÓÒ×ÓÊ÷
+int childLength(int n, int m, int len);
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	int len = 1;
+	int res;
+	while (n != 0 && m != 0)
+	{
+		res = childLength(n, m, 1);
+		cout << res << endl;
+		cin >> n >> m;
+	}
+	return 0;
+}
+int childLength(int n, int m, int len)
+{
+	if (n * 2 <= m)
+	{
+		len++;
+		len = childLength(n * 2, m, len);
+	}
+	if (n * 2 + 1<= m)
+	{
+		len++;
+		len = childLength(n * 2 + 1, m, len);
+	}
+	return len;
+}
